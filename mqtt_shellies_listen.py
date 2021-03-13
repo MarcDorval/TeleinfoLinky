8f102c8375068aabaa6b9917290eb432ca76925a
Marc Dorval <marc.dorval35@gmail.com>
2021-03-13 18:34:00 +0100

    Adding shellies listener

---
+++ mqtt_shellies_listen.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   Test du broker mqtt pour teleinformation linky
+   la fonction 'on_message' sera appelée à chaque mise à jour d'un des topics listés 
+#  use case: python3 mqtt_linky_listen.py
+"""
+
+import random
+import time
+import sys
+
+from mymqtt import myMqtt
+from paho.mqtt import client as mqtt_client
+
+broker="PiCuisine"
+
+mqttc = myMqtt("listen_shellies")
+mqttc.connect_to(broker)
+
+mqttc.subscribe([("shellies/#", 1)])
+mqttc.listen()
