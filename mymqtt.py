9ace483f3020a378731b2fd97cba9b2e2b610f2f
Marc Dorval <marc.dorval35@gmail.com>
2021-03-14 13:00:15 +0100

    Adding logger

--- mymqtt.py
+++ mymqtt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import datetime
+import logging
 from paho.mqtt import client as mqtt_client
+logging.basicConfig(filename=f"mqtt.log", level=logging.DEBUG)
 
 class myMqtt():
     """
@@ -14,20 +16,24 @@ class myMqtt():
     def __init__(self, client_id):
         self.client_id = client_id
         self.client = mqtt_client.Client(client_id)
-        print(self.client)
+        # print(self.client)
+        self.log = logging.getLogger(client_id)
+        #self.log.basic(filename=f"{log_client_id}.txt", encoding='utf-8', level=logging.DEBUG)
+        self.client.enable_logger(self.log)
+        self.log.info(f"init complete")
 
     def on_connect(self, mqtt_client_id, userdata, flags, rc):
         ymdhms = self.yyyymmddhhmmss()
         if rc == 0:
-            print(f"{ymdhms}: {self.client_id} Connected to MQTT Broker {self.broker}")
+            self.log.info(f"{ymdhms}: {self.client_id} Connected to MQTT Broker {self.broker}")
         else:
-            print(f"{ymdhms}: {self.client_id} Failed to connect to MQTT Broker {self.broker}, return code %d\n", rc)
+            self.log.info(f"{ymdhms}: {self.client_id} Failed to connect to MQTT Broker {self.broker}, return code %d\n", rc)
 
     def on_socket_open(self):
-        print(f"socket open")
+        self.log.info(f"socket open")
 
     def on_socket_close(self):
-        print(f"socket close")
+        self.log.info(f"socket close")
 
     def connect_to(self, broker, port=1883):
         ymdhms = self.yyyymmddhhmmss()
@@ -42,16 +48,16 @@ class myMqtt():
     def on_message(self, c, userdata, msg):
         ymdhms = self.yyyymmddhhmmss()
         if "last will" in msg.topic:
-            print(f"{ymdhms} < {msg.topic}: {msg.payload.decode()}")
+            self.log.info(f"{ymdhms} < {msg.topic}: {msg.payload.decode()}")
         else:
-            print(f"{ymdhms} < {msg.topic}: {msg.payload.decode()}")
+            self.log.info(f"{ymdhms} < {msg.topic}: {msg.payload.decode()}")
 
     def yyyymmddhhmmss(self):
         now = datetime.datetime.now()
         return now.strftime("%Y-%m-%d %H:%M:%S")
 
     def on_log(self, userdata, level, buf):
-        print("log: ", buf)
+        self.log.info("log: {buf}")
 
     def subscribe(self, topics, qos=1):
         self.client.subscribe(topics, qos)
@@ -65,12 +71,13 @@ class myMqtt():
         result = self.client.publish(topic, msg, retain)
         status = result[0]
         if status == 0:
-            print(f"{self.client_id} > {topic}: {msg}")
+            self.log.info(f"{self.client_id} > {topic}: {msg}")
         else:
-            print(f"Failed to send message to topic {topic}")
+            self.log.info(f"Failed to send message to topic {topic}")
 
     def listen(self):
         self.client.loop_forever(timeout=1.0)
 
     def deaf(self):
+        self.log.info(f"deaf??")
         self.client.loop_stop()
